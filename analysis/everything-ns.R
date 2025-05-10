85d6d9fe1c15f6bd82cfec378c228aca10a5f1fc
Kanishka Misra <menogetusername@gmail.com>
2025-05-10 17:09:20 -0500

    qwen correct on no question ids

--- analysis/everything-ns.R
+++ analysis/everything-ns.R
@@ -45,10 +45,16 @@ real_model_meta <- tribble(
   "qwen-2.5-7b-instruct", "Qwen2.5-I vs. Qwen2.5-VL-I"
 )
 
-results_raw <- read_csv("~/Downloads/updated_output_merged_strict_eval.csv") 
+# results_raw <- read_csv("~/Downloads/updated_output_merged_strict_eval.csv") 
+results_raw <- read_tsv("~/Downloads/merged_model_results.csv") 
+
+valid_types <- results_raw %>% 
+  filter(substitution_hop <0) %>% 
+  count(question_type) %>%
+  pull(question_type)
 
 longer <- results_raw %>%
-  select(-question, -scene_description, -ground_truth) %>%
+  select(-question, -input, -ground_truth) %>%
   pivot_longer(lm_Llama_3.1_8B:vlm_text_qwen2.5VL, names_to = "model_setting", values_to = "outcome") %>%
   mutate(
     is_ns = case_when(
@@ -66,7 +72,7 @@ longer <- results_raw %>%
       TRUE ~ substitution_hop
     )
   ) %>%
-  filter(question_type == "exist")
+  filter(question_type %in% valid_types)
 
 ns_results <- longer %>%
   filter(is_ns == TRUE) %>%
@@ -96,10 +102,10 @@ pos_results <- longer %>%
     )
   )
 
-
 with_ns <- ns_results %>%
+  rename(neg_outcome = outcome) %>%
   inner_join(pos_results %>% rename(pos_outcome = outcome)) %>%
-  mutate(correct = (outcome == 1 & pos_outcome == 1))
+  mutate(correct = (neg_outcome == 1 & pos_outcome == 1))
 
 overall_results <- with_ns %>%
   group_by(model_setting) %>%
@@ -127,12 +133,44 @@ conditional <- with_ns %>%
   mutate(
     og_correct = TRUE
   ) %>%
-  inner_join(longer %>% filter(substitution_hop != 0))
+  inner_join(with_ns %>% filter(substitution_hop != 0))
+
+conditional_new <- with_ns %>%
+  filter(substitution_hop == 0) %>%
+  select(question_id, model_setting, og_correct = correct) %>%
+  inner_join(with_ns) %>%
+  mutate(
+    cond_select = case_when(
+      og_correct == FALSE & substitution_hop > 0 ~ FALSE,
+      TRUE ~ TRUE
+    )
+  ) %>%
+  filter(cond_select == TRUE)
+  
+
+# with_ns %>%
+#   filter(substitution_hop == 0) %>%
+#   filter(correct == TRUE) %>% count(question_id, model_setting, sort=TRUE)
 
 conditional_results <- conditional %>%
   group_by(model_setting) %>%
   summarize(
-    outcome = mean(outcome==TRUE)
+    outcome = mean(correct==TRUE)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    setting = str_extract(model_setting, "(vlm_q_only_|vlm_text_|vlm_|lm_q_only_|lm_)"),
+    model = str_remove(model_setting, "(vlm_q_only_|vlm_text_|vlm_|lm_q_only_|lm_)")
+  ) %>%
+  filter(!setting %in% c("vlm_q_only_", "vlm_", "lm_q_only_")) %>%
+  inner_join(model_meta) %>%
+  select(class, type, outcome) %>%
+  pivot_wider(names_from = type, values_from = outcome, values_fill = 0)
+
+conditional_new_results <- conditional_new %>%
+  group_by(model_setting) %>%
+  summarize(
+    outcome = mean(correct==TRUE)
   ) %>%
   ungroup() %>%
   mutate(
@@ -148,7 +186,7 @@ conditional_results <- conditional %>%
 hca_results <- with_ns %>%
   group_by(question_id, model_setting) %>% 
   summarize(
-    outcome = sum(outcome == TRUE),
+    outcome = sum(correct == TRUE),
     n = n()
   ) %>%
   ungroup() %>%
@@ -166,17 +204,18 @@ hca_results <- with_ns %>%
 
 bind_rows(
   hca_results %>% mutate(metric = "HCA"),
-  overall_results %>% mutate(metric = "Conditional"),
-  conditional_results %>% mutate(metric = "Overall")
+  overall_results %>% mutate(metric = "Overall"),
+  conditional_results %>% mutate(metric = "Conditional"),
+  conditional_new_results %>% mutate(metric = "Conditional (New)")
 ) %>%
   mutate(
-    metric = factor(metric, levels = c("Overall", "Conditional", "HCA"))
+    metric = factor(metric, levels = c("Overall", "Conditional (New)", "Conditional", "HCA"))
   ) %>%
   inner_join(real_model_meta) %>%
   ggplot(aes(`Text Only`, `Vision + Text`, color = pair, shape = pair, fill = pair)) +
   geom_point(size = 3) +
   geom_abline(slope = 1, linetype = "dashed", linewidth = 0.2) +
-  facet_wrap(~metric) +
+  facet_wrap(~metric, nrow = 1) +
   scale_shape_manual(values = c(21, 22, 23, 24, 25, 8, 9)) +
   scale_color_brewer(palette = "Dark2", aesthetics = c("color", "fill")) +
   scale_x_continuous(limits = c(0,1)) +
@@ -194,6 +233,7 @@ bind_rows(
 
 ns_results_cat <- longer %>%
   filter(is_ns == TRUE) %>%
+  # group_by(question_id, question_type, model_setting, substitution_hop, original_arg) %>%
   group_by(question_id, model_setting, substitution_hop, original_arg) %>%
   summarize(
     outcome = mean(outcome == TRUE)
@@ -208,6 +248,7 @@ ns_results_cat <- longer %>%
 
 pos_results_cat <- longer %>%
   filter(is_ns == FALSE) %>%
+  # group_by(question_id, question_type, model_setting, substitution_hop, original_arg) %>%
   group_by(question_id, model_setting, substitution_hop, original_arg) %>%
   summarize(
     outcome = mean(outcome == TRUE)
@@ -220,21 +261,125 @@ pos_results_cat <- longer %>%
     )
   )
 
-
 with_ns_cat <- ns_results_cat %>%
+  rename(neg_outcome = outcome) %>%
   inner_join(pos_results_cat %>% rename(pos_outcome = outcome)) %>%
-  mutate(correct = (outcome == 1 & pos_outcome == 1))
+  mutate(correct = (neg_outcome == 1 & pos_outcome == 1))
 
 
-with_ns_cat %>%
-  group_by(question_id, model_setting, original_arg) %>% 
+# with_ns_cat <- ns_results_cat %>%
+#   inner_join(pos_results_cat %>% rename(pos_outcome = outcome)) %>%
+#   mutate(correct = (outcome == 1 & pos_outcome == 1))
+
+# how often does an object occur for boht types of questions
+arg_both <- results_raw %>% 
+  filter(substitution_hop >=0) %>% 
+  count(question_type, ground_truth, original_arg) %>%
+  filter(question_type %in% valid_types) %>%
+  count(original_arg, ground_truth) %>%
+  count(original_arg) %>%
+  mutate(
+    both = (n > 1)
+  ) %>%
+  select(-n)
+
+longer_gt <- results_raw %>%
+  select(-question, -input) %>%
+  pivot_longer(lm_Llama_3.1_8B:vlm_text_qwen2.5VL, names_to = "model_setting", values_to = "outcome") %>%
+  mutate(
+    is_ns = case_when(
+      substitution_hop < 0 ~ TRUE,
+      TRUE ~ FALSE
+    ),
+    substitution_hop = case_when(
+      # substitution_hop >= 0 ~ substitution_hop,
+      substitution_hop == -100 ~ 0,
+      substitution_hop == -1 ~ 1,
+      substitution_hop == -2 ~ 2,
+      substitution_hop == -3 ~ 3,
+      substitution_hop == -4 ~ 4,
+      substitution_hop == -5 ~ 5,
+      TRUE ~ substitution_hop
+    )
+  ) %>%
+  filter(question_type %in% valid_types)
+
+ns_results_cat_no <- longer_gt %>%
+  filter(is_ns == TRUE) %>%
+  group_by(question_id, model_setting, substitution_hop, original_arg) %>%
+  # group_by(question_id, model_setting, substitution_hop, original_arg) %>%
   summarize(
-    outcome = sum(outcome == TRUE),
-    n = n()
+    outcome = mean(outcome == TRUE)
   ) %>%
   ungroup() %>%
+  mutate(
+    outcome = case_when(
+      outcome != 1 ~ 0,
+      outcome == 1 ~ 1
+    )
+  )
+
+pos_results_cat_no <- longer_gt %>%
+  filter(is_ns == FALSE) %>%
+  # group_by(question_id, ground_truth, model_setting, substitution_hop, original_arg) %>%
+  group_by(question_id, model_setting, substitution_hop, original_arg) %>%
+  summarize(
+    outcome = mean(outcome == TRUE)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    outcome = case_when(
+      outcome != 1 ~ 0,
+      outcome == 1 ~ 1
+    )
+  )
+
+pos_no <- longer_gt %>%
+  filter(is_ns == FALSE) %>%
+  filter(ground_truth == "no") %>%
+  distinct(question_id) %>%
+  pull(question_id)
+
+with_ns_cat_no <- ns_results_cat_no %>%
+  rename(neg_outcome = outcome) %>%
+  inner_join(pos_results_cat_no %>% rename(pos_outcome = outcome)) %>%
+  mutate(correct = (neg_outcome == 1 & pos_outcome == 1)) %>%
+  filter(question_id %in% pos_no)
+
+
+# arg_no <- with_ns_cat_no %>% 
+#   filter(model_setting == "lm_Llama_3.1_8B") %>%
+#   filter(substitution_hop >=0) %>% 
+#   count(ground_truth, original_arg) %>%
+#   filter(ground_truth == "no") %>%
+#   select(-ground_truth) %>%
+#   rename(n_no = n)
+
+results_raw %>%
+  filter(question_type %in% valid_types) %>%
+  count(question_type, ground_truth)
+
+
+conditional_cat <- with_ns_cat_no %>%
+  filter(substitution_hop == 0) %>%
+  filter(correct == TRUE) %>%
+  select(question_id, original_arg, model_setting) %>%
+  mutate(
+    og_correct = TRUE
+  ) %>%
+  inner_join(with_ns_cat_no %>% filter(substitution_hop != 0))
+
+conditional_cat %>% filter(str_detect(model_setting, "(qwen2.5|Qwen2.5)")) %>%
+  write_csv("data/gqa_dataset/qwen-correct.csv")
+
+# no only
+
+conditional_cat %>%
   group_by(model_setting, original_arg) %>%
-  summarize(outcome = mean(outcome == n)) %>%
+  summarize(
+    outcome = mean(correct==TRUE),
+    n = n()
+  ) %>%
   ungroup() %>%
   mutate(
     setting = str_extract(model_setting, "(vlm_q_only_|vlm_text_|vlm_|lm_q_only_|lm_)"),
@@ -242,10 +387,39 @@ with_ns_cat %>%
   ) %>%
   filter(!setting %in% c("vlm_q_only_", "vlm_", "lm_q_only_")) %>%
   inner_join(model_meta) %>%
-  select(class, original_arg, type, outcome) %>%
-  pivot_wider(names_from = type, values_from = outcome, values_fill = 0) %>%
+  select(class, original_arg, type, outcome, n) %>%
+  pivot_wider(names_from = type, values_from = c(outcome, n)) %>%
   mutate(
-    diff = `Vision + Text` - `Text Only`
-  ) %>% inner_join(results_raw %>% filter(question_type == "exist") %>% count(original_arg)) %>% View()
+    diff = `outcome_Vision + Text` - `outcome_Text Only`
+  ) %>%
+  View()
+  
+
+# with_ns_cat %>%
+#   # group_by(question_id, question_type, model_setting, original_arg) %>% 
+#   group_by(question_id, model_setting, original_arg) %>% 
+#   summarize(
+#     outcome = sum(correct == TRUE),
+#     n = n()
+#   ) %>%
+#   ungroup() %>%
+#   # group_by(model_setting, question_type, original_arg) %>%
+#   group_by(model_setting, original_arg) %>%
+#   summarize(outcome = mean(outcome == n)) %>%
+#   ungroup() %>%
+#   mutate(
+#     setting = str_extract(model_setting, "(vlm_q_only_|vlm_text_|vlm_|lm_q_only_|lm_)"),
+#     model = str_remove(model_setting, "(vlm_q_only_|vlm_text_|vlm_|lm_q_only_|lm_)")
+#   ) %>%
+#   filter(!setting %in% c("vlm_q_only_", "vlm_", "lm_q_only_")) %>%
+#   inner_join(model_meta) %>%
+#   # select(class, question_type, original_arg, type, outcome) %>%
+#   select(class, original_arg, type, outcome) %>%
+#   pivot_wider(names_from = type, values_from = outcome, values_fill = 0) %>%
+#   mutate(
+#     diff = `Vision + Text` - `Text Only`
+#   ) %>% inner_join(results_raw %>% filter(question_type %in%valid_types) %>% count(original_arg)) %>% 
+#   inner_join(arg_both) %>% View()
+
 
 
