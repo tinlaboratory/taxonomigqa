ea71daef5786fbf39d3c4b85bef0e0758f434f90
Yulu Qin <yuluqin@bu.edu>
2025-07-16 18:16:22 -0400

    upload the .sh file to run inference

---
+++ src/prompting/submit_single_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#$ -P tin-lab       # Specify the SCC project name you want to use
+#$ -l h_rt=6:00:00   # Specify the hard time limit for the job
+#$ -N model_inference         # Give job a name
+#$ -o /projectnb/tin-lab/yuluq/data/subset_combined_stats_data/output_file/testing_2b.txt             # output file name
+#$ -l gpus=1
+#$ -l gpu=1
+#$ -l gpu_memory=80G
+#$ -j y               # Merge the error and output streams into a single file
+
+module load miniconda
+conda activate /projectnb/tin-lab/yuluqin/multimodal_semantic
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+source ~/.bashrc
+python /projectnb/tin-lab/yuluq/multimodal-representations/src/prompting/run_inference.py --config="$1"
